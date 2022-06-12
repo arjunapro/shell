bf3e821681e815e5d003c83e007fa747267dd910
Andrey <asternov@icloud.com>
2022-06-12 19:30:26 +0530

    commit

--- dist/_nuxt/static/1655034327/blog/2019-09-22-veggies/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1655034327",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002F2019-09-22-veggies",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"4c039e26"}},_img:{"/_ipx/_/author.jpg":"\u002F_nuxt\u002Fimage\u002Fd17f97.jpg"}}}(null));
