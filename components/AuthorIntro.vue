50417e355c954c02433bc03ee25b952e26cc718e
Andrey <asternov@icloud.com>
2022-06-16 19:49:13 +0530

    commit

--- components/AuthorIntro.vue
+++ components/AuthorIntro.vue
@@ -3,7 +3,7 @@
     <h2
       class="mt-6 text-4xl text-gray-700 font-bold capitalize dark:text-blue-200"
     >
-      👋 Welcome!
+      👋 Welcome, guy!
     </h2>
     <p class="mt-8 mb-4 dark:bg-gray-800 dark:text-blue-200 text-justify">
       My name is Andrew. I am a PHP developer with more than 7 years of experience<br>
