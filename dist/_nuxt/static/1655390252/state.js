581a3d4f9d8bfa99e5e6319972f878e9efbee734
Andrey <asternov@icloud.com>
2022-06-16 20:25:36 +0530

    commit

--- dist/_nuxt/static/1655390252/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1655390252",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"740098bc"}},_img:{"/_ipx/_/author.jpg":"\u002F_nuxt\u002Fimage\u002Fd17f97.jpg"}}}(null,"\u002F"));
---
+++ dist/_nuxt/static/1655391320/blog/2022-05-25-phpgrad/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1655391320",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002F2022-05-25-phpgrad",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f39a43b3"}},_img:{"/_ipx/_/author.jpg":"\u002F_nuxt\u002Fimage\u002Fd17f97.jpg"}}}(null));
