22ed7db99dc9fea9ae54626115e9e0bbc832ed5d
Andrey <asternov@icloud.com>
2022-06-16 20:08:03 +0530

    commit

--- dist/_nuxt/static/1655390197/blog/2022-05-25-phpgrad/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1655390197",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002F2022-05-25-phpgrad",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8454c9ed"}},_img:{"/_ipx/_/author.jpg":"\u002F_nuxt\u002Fimage\u002Fd17f97.jpg"}}}(null));
