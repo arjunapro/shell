6f76d98fd025c81d7d8045e4c8b28796dfb94924
Andrey <asternov@icloud.com>
2022-06-19 15:29:40 +0530

    commit

---
+++ dist/_nuxt/static/1655632767/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1655632767",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7e234402"}},_img:{"/_ipx/_/author.jpg":"\u002F_nuxt\u002Fimage\u002Fd17f97.jpg"}}}(null,"\u002F"));
